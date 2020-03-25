f3a8a488f2bc0ba78fca11305e57b408abbd0fcd
carlosprietobarron <carlosprietobarron@gmail.com>
2020-03-24 19:12:56 -0700

    Create my_each and my_each_with_index methods

---
+++ main.rb
@@ -0,0 +1,26 @@
+module Enumerable
+    def my_each
+        i = 0
+        length.times do
+          yield (self[i])
+          i += 1
+        end
+      end
+    
+      def my_each_with_index
+        i = 0
+        length.times do
+          yield(self[i], i)
+          i += 1
+        end
+      end
+end
+
+
+# control
+puts 'my each'
+array_ctl = [9, 3, 5, 8, 1]
+array_ctl.my_each { |i| puts i }
+puts 'my each with index'
+array_ctl.my_each_with_index { |item, i| puts item.to_s + ' ' + i.to_s }
+array_str = %w[hola 3244 adios valida 344 _er tambien]
